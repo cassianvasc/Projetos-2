24d9c4c0bc639e7b04412f987db9df5772d84b4d
VitorCSa <vitorcarvalhosa12@gmail.com>
2025-11-18 13:22:25 -0300

    Adiciona imagens no models de noticias

--- Jornalista/models.py
+++ Jornalista/models.py
@@ -14,6 +14,7 @@ class Noticia(models.Model):
     autor = models.ForeignKey(Perfil, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=50)
     conteudo = RichTextField()
+    imagem = models.ImageField(upload_to='noticias/', blank=True, null=True)
     data_postagem = models.DateTimeField(auto_now_add=True)
     data_atualizada = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField('athena.Tag', related_name='NoticiaComTag',blank=True)
--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -135,6 +134,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
