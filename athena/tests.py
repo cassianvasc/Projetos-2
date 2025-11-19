165a9dead11c7a5434bf2214e86250504078c581
VitorCSa <vitorcarvalhosa12@gmail.com>
2025-11-19 11:11:18 -0300

    Adiciona os testes

--- athena/tests.py
+++ athena/tests.py
@@ -1,57 +1,13 @@
-'''
+from django.test import TestCase,LiveServerTestCase,override_settings
+from Jornalista.models import Perfil as perfilJornalista
+from .models import Perfil as perfilUsuario
+from selenium.webdriver.common.action_chains import ActionChains
 from django.contrib.auth.models import User
+from django.urls import reverse
+from Jornalista.models import *
+from .models import *
 import time
 
-from django.urls import reverse
-from .models import Noticia, Tag 
-
-        
-    def setUp(self):
-        # Cria Tags de teste
-        self.tag_politica = Tag.objects.create(nome='Política Local')
-        self.tag_economia = Tag.objects.create(nome='Economia')
-        
-        # Cria Notícias de teste e as associa às Tags
-        self.noticia_a = Noticia.objects.create(titulo='Título A', conteudo='Conteúdo A')
-        self.noticia_b = Noticia.objects.create(titulo='Título B', conteudo='Conteúdo B')
-        self.noticia_c = Noticia.objects.create(titulo='Título C', conteudo='Conteúdo C')
-        
-        # Associa Tags:
-        self.noticia_a.tags.add(self.tag_politica) # Noticia A é Política
-        self.noticia_b.tags.add(self.tag_politica, self.tag_economia) # Noticia B é Política E Economia
-        self.noticia_c.tags.add(self.tag_economia) # Noticia C é só Economia
-
-  
-    def test_noticias_por_tag_filtra_corretamente(self):
-        
-        # a) Simula o acesso à URL da tag "Política Local" (o slug é gerado automaticamente)
-        response_politica = self.client.get(
-            reverse('noticias_por_tag', args=[self.tag_politica.slug])
-        )
-        
-        # Verifica se o status HTTP é 200 (Sucesso na exibição da página)
-        self.assertEqual(response_politica.status_code, 200)
-        
-        # Verifica se as notícias corretas estão na resposta (A e B devem ser encontradas)
-        self.assertIn(self.noticia_a, response_politica.context['noticias'])
-        self.assertIn(self.noticia_b, response_politica.context['noticias'])
-        
-        # Verifica se a notícia incorreta NÃO está na resposta (C não deve ser encontrada)
-        self.assertNotIn(self.noticia_c, response_politica.context['noticias'])
-
-        
-   
-    def test_noticias_por_tag_slug_invalido(self):
-        
-        # Simula o acesso a um slug que não existe no banco
-        response = self.client.get(
-            reverse('noticias_por_tag', args=['tag-que-nao-existe'])
-        )
-        
-        # Espera um erro 404 (Not Found), que é o comportamento do get_object_or_404
-        self.assertEqual(response.status_code, 404)
-'''
-from django.test import TestCase,LiveServerTestCase,override_settings
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -70,8 +26,6 @@ class TesteE2E(LiveServerTestCase):
     def setUpClass(cls):
         super().setUpClass()
         options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')        
         options.add_argument("--disable-infobars")
         options.add_argument("--incognito")
@@ -113,7 +67,7 @@ class TesteE2E(LiveServerTestCase):
 
         self.browser.get(f'{self.live_server_url}/register/')
 
-        self.assertIn('Digite um login:',self.browser.page_source)
+        self.assertIn('Já tem uma conta?',self.browser.page_source)
 
         usernameInput = self.browser.find_element(By.NAME,'username')
         emailInput = self.browser.find_element(By.NAME,'email')
@@ -123,7 +77,9 @@ class TesteE2E(LiveServerTestCase):
         emailInput.send_keys('a@gmail.com')
         passwordInput.send_keys('123')
         passwordInput.send_keys(Keys.RETURN)
+        time.sleep(5)
 
+        print("Checando se existe usuario")
         self.assertTrue(User.objects.filter(username='teste').exists())
 
         WebDriverWait(self.browser, 10).until(
@@ -137,9 +93,10 @@ class TesteE2E(LiveServerTestCase):
         passwordInput.send_keys('123')
         passwordInput.send_keys(Keys.RETURN)
 
+        print(self.browser.page_source)
+
         WebDriverWait(self.browser,10).until(
-            expected_conditions.text_to_be_present_in_element(
-                (By.TAG_NAME, "body"), "Nome de usuario ou senha incorreto")
+            expected_conditions.presence_of_element_located((By.NAME,'erro'))
         )
 
         usernameInput = self.browser.find_element(By.NAME,'username')
@@ -150,7 +107,7 @@ class TesteE2E(LiveServerTestCase):
         passwordInput.send_keys(Keys.RETURN)
 
         WebDriverWait(self.browser, 10).until(
-            expected_conditions.presence_of_element_located((By.NAME,'user'))
+            expected_conditions.presence_of_element_located((By.CLASS_NAME,'page-wrapper'))
         )
 
     def test_tags(self):
@@ -166,31 +123,122 @@ class TesteE2E(LiveServerTestCase):
             expected_conditions.url_contains('/user/')
         )
 
-        futebolButton = self.browser.find_element(By.CSS_SELECTOR,f'input[type="checkbox"][value="{ futebolTag.id }"]')
-        politicaButton = self.browser.find_element(By.CSS_SELECTOR,f'input[type="checkbox"][value="{ politicaTag.id }"]')
+        futebolButton = self.browser.find_element(By.XPATH, "//label[.//span[text()='Futebol']]")
+        politicaButton = self.browser.find_element(By.XPATH, "//label[.//span[text()='Politica']]")
         saveButton = self.browser.find_element(By.NAME,"save")
 
         futebolButton.click()
         politicaButton.click()
         saveButton.click()
 
-        self.browser.get(f'{self.live_server_url}/')
-
         WebDriverWait(self.browser, 10).until(
-            expected_conditions.presence_of_element_located((By.NAME,'user'))
+            expected_conditions.presence_of_element_located((By.NAME,'context'))
         )
 
-        button = self.browser.find_element(By.NAME,"user")
-        button.click()
+        self.assertEqual(user.perfil.tags.count(), 2)
 
-        WebDriverWait(self.browser, 10).until(
-            expected_conditions.url_contains('/user/')
-        )
-
-        futebolButton = self.browser.find_element(By.CSS_SELECTOR,f'input[type="checkbox"][value="{ futebolTag.id }"]')
+        futebolButton = self.browser.find_element(By.XPATH, "//label[.//span[text()='Futebol']]")
         saveButton = self.browser.find_element(By.NAME,"save")
 
         futebolButton.click()
         saveButton.click()
+        time.sleep(1)
+        self.assertEqual(user.perfil.tags.count(),1)
+
+
+    def test_noticias_por_tag_slug_invalido(self):
+        
+        response = self.client.get(
+            reverse('noticias_por_tag', args=['tag-que-nao-existe'])
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_noticias_por_tag_filtra_corretamente(self):
+
+        user = User.objects.create_user(username='jornalistaTeste')
+        jornalista = perfilJornalista.objects.create(user=user)
+
+        self.tag_politica = Tag.objects.create(nome='Política Local')
+        self.tag_economia = Tag.objects.create(nome='Economia')
+        
+        self.noticia_a = Noticia.objects.create(autor=jornalista,titulo='Título A', conteudo='Conteúdo A')
+        self.noticia_b = Noticia.objects.create(autor=jornalista,titulo='Título B', conteudo='Conteúdo B')
+        self.noticia_c = Noticia.objects.create(autor=jornalista,titulo='Título C', conteudo='Conteúdo C')
+        
+        self.noticia_a.tags.add(self.tag_politica) 
+        self.noticia_b.tags.add(self.tag_politica, self.tag_economia)
+        self.noticia_c.tags.add(self.tag_economia)
+
+        self.browser.get(f'{self.live_server_url}/')
+        print(self.browser.page_source)
+
+        noticia = self.browser.find_element(
+            By.XPATH,
+            f"//a[@class='read-more-btn' and @href='/noticia/{self.noticia_a.id}/']"
+        )
+        actions = ActionChains(self.browser)
+        actions.move_to_element(noticia).perform()
+        noticia.click()
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.url_contains('/noticia/')
+        )
+
+        response_politica = self.client.get(
+            reverse('noticias_por_tag', args=[self.tag_politica.slug])
+        )
+
+        tag = self.browser.find_element(By.XPATH,f"//a[contains(@href, '/tag/{self.tag_politica.slug}')]")
+        tag.click()
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.url_contains('/tag/')
+        )
+
+        self.assertEqual(response_politica.status_code, 200)
+        
+        self.assertIn(self.noticia_a, response_politica.context['noticias'])
+        self.assertIn(self.noticia_b, response_politica.context['noticias'])
+        
+        self.assertNotIn(self.noticia_c, response_politica.context['noticias'])
+
+    def test_pesquisa_noticia(self):
+
+        user = User.objects.create_user(username='jornalistaTeste')
+        jornalista = perfilJornalista.objects.create(user=user)
+
+        self.noticia_a = Noticia.objects.create(autor=jornalista,titulo='Título A', conteudo='Conteúdo A')
+
+        self.browser.get(f'{self.live_server_url}/')
+
+        search = self.browser.find_element(By.NAME,"BarraDePesquisa")
+        searchButton = self.browser.find_element(By.CLASS_NAME,"search-btn")
+        search.send_keys("Título A")
+        searchButton.click()
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.url_contains('/pesquisa/')
+        )
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.presence_of_element_located((By.NAME,f"{self.noticia_a.titulo}"))
+        )
+
+        self.browser.get(f'{self.live_server_url}/')
+
+        search = self.browser.find_element(By.NAME,"BarraDePesquisa")
+        searchButton = self.browser.find_element(By.CLASS_NAME,"search-btn")
+        search.send_keys("nada")
+        searchButton.click()
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.url_contains('/pesquisa/')
+        )
+
+        WebDriverWait(self.browser, 10).until(
+            expected_conditions.presence_of_element_located((By.NAME,"context"))
+        )
 
 # Create your tests here.
